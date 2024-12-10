48d2aef974366381e1516a64258d14e017903f68
jonlaasa <113533659+jonlaasa@users.noreply.github.com>
2024-12-10 22:04:14 +0100

    Delete Django directory

--- Django/IoTproject/appIoTproject/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Paciente, Familiar, PacienteFamiliarUnion
-admin.site.register(Paciente)
-admin.site.register(Familiar)
-admin.site.register(PacienteFamiliarUnion)
